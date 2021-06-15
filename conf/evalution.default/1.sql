5d7662ad4dd161bc40799ded7a4d75180fe4bf53
Bekturdi <yuldashevbekturdi@gmail.com>
2021-06-15 12:21:13 +0500

    application.conf configure for DB

--- conf/evalution.default/1.sql
+++ conf/evalution.default/1.sql
@@ -1,7 +1,7 @@
-CREATE TABLE "Example" (
-    "id" SERIAL NOT NULL PRIMARY KEY,
-    "name" VARCHAR NOT NULL
-);
-ALTER TABLE "Example" add column "tel" varchar null;
-ALTER TABLE "Example" add column "age" varchar null;
-ALTER TABLE "Example" add column "address" varchar null;
+-- CREATE TABLE "Example" (
+--     "id" SERIAL NOT NULL PRIMARY KEY,
+--     "name" VARCHAR NOT NULL
+-- );
+-- ALTER TABLE "Example" add column "tel" varchar null;
+-- ALTER TABLE "Example" add column "age" varchar null;
+-- ALTER TABLE "Example" add column "address" varchar null;
